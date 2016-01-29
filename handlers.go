e42cd979409af6a8b9f89d34366381df4d98cafe
Vladimir Osintsev <osintsev@gmail.com>
2016-01-29 15:44:36 +0300

    API scaffolding

--- handlers.go
+++ handlers.go
@@ -3,8 +3,45 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"database/sql"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, heroku")
+}
+
+func GetBeerListHandler(db *sql.DB) http.Handler {
+	return http.Handler(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func GetBeerByKeyHandler(db *sql.DB) http.Handler {
+	return http.Handler(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func GetBrandListHandler(db *sql.DB) http.Handler {
+	return http.Handler(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func GetBreweryKeyHandler(db *sql.DB) http.Handler {
+	return http.Handler(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func GetBreweryListHandler(db *sql.DB) http.Handler {
+	return http.Handler(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func GetBrandByKeyHandler(db *sql.DB) http.Handler {
+	return http.Handler(func(w http.ResponseWriter, r *http.Request) {
+
+	})
 }
--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"os"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
+
+var db *sql.DB
+
+const dbType = "mysql"
+
+func init() {
+	dsn := strings.TrimPrefix(os.Getenv("CLEARDB_DATABASE_URL"), dbType+"://")
+	db, _ = sql.Open(dbType, dsn)
+	if err := db.Ping(); err != nil {
+		log.Fatal("Can't connect to database")
+		os.Exit(1)
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
+	api := mux.NewRouter().StrictSlash(true)
 
+	api.HandleFunc("beer", GetBeerListHandler(db))
+	api.HandleFunc("beer/{key}", GetBeerByKeyHandler(db))
+	api.HandleFunc("brand", GetBrandListHandler(db))
+	api.HandleFunc("brand/{key}", GetBrandByKeyHandler(db))
+	api.HandleFunc("brewery", GetBreweryListHandler(db))
+	api.HandleFunc("brewery/{key}", GetBreweryKeyHandler(db))
+
+	http.Handle("/api", api)
 	http.HandleFunc("/", HomeHandler)
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
