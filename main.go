19ce27bd3fa87d0131cb3a16444a4c36f921959f
Vladimir Osintsev <osintsev@gmail.com>
2016-03-25 12:53:25 +0300

    Refactoring

--- main.go
+++ main.go
@@ -26,27 +26,25 @@ import (
 
 func main() {
 	// Database initialization
-	err := InitDB()
-	if err != nil {
+	if err := InitDB(); err != nil {
 		log.Fatalf("DB error: %v", err)
-		os.Exit(1)
 	}
 	defer db.DB().Close()
 
 	// HTTP routes
-	r := mux.NewRouter()
-	r.HandleFunc("/", ReadmeHandler).Methods("GET")
-	api := r.PathPrefix("/api/v1").Subrouter()
-	api.Handle("/beer", GetBeerList(&db))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", ReadmeHandler).Methods("GET")
+
+	api := router.PathPrefix("/api/v1").Subrouter()
+	api.Handle("/beer/", GetBeerList(&db))
 	api.Handle("/beer/{key}", GetBeerByKey(&db))
-	api.Handle("/brand", GetBrandList(&db))
+	api.Handle("/brand/", GetBrandList(&db))
 	api.Handle("/brand/{key}", GetBrandByKey(&db))
-	api.Handle("/brewery", GetBreweryList(&db))
+	api.Handle("/brewery/", GetBreweryList(&db))
 	api.Handle("/brewery/{key}", GetBreweryKey(&db))
-	http.Handle("/", r)
 
 	// Start server
 	port := os.Getenv("PORT")
 	log.Printf("Starting beerdb-server on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
